09bee0b41cd15fd467a61f1bf3dc2435fb352a85
aamirhussain1911 <luckyxyz021@gmail.com>
2026-09-18 23:03:15 +0530

    Add files via upload

---
+++ question_029.c
@@ -0,0 +1,26 @@
+//Q29: Write a program to calculate the factorial of a number.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    unsigned long long factorial = 1;
+
+    
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+
+    
+    if (n < 0) {
+        printf("Error: Factorial of a negative number does not exist.\n");
+    } else {
+    
+        for (int i = 1; i <= n; i++) {
+            factorial *= i; 
+        }
+
+        printf("The factorial of %d is: %llu\n", n, factorial);
+    }
+
+    return 0;
+}
