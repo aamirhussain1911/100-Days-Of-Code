fdf2c05d0e9307594dab656665a5ce9f315eb1fc
aamirhussain1911 <luckyxyz021@gmail.com>
2026-09-18 23:07:37 +0530

    Add files via upload

---
+++ question_030.c
@@ -0,0 +1,26 @@
+// Q30: Write a program to reverse a given number.
+
+#include <stdio.h>
+
+int main() {
+    int num, reversedNum = 0, remainder;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    
+    int originalNum = num;
+
+    
+    while (num != 0) {
+        remainder = num % 10;                     
+        reversedNum = reversedNum * 10 + remainder; 
+        num = num / 10;                           
+    }
+
+    printf("Original number: %d\n", originalNum);
+    printf("Reversed number: %d\n", reversedNum);
+
+    return 0;
+}
