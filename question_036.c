36893982b09330b81d0b04497388eb210d92747c
aamirhussain1911 <luckyxyz021@gmail.com>
2026-09-21 19:29:17 +0530

    Add files via upload

---
+++ question_036.c
@@ -0,0 +1,26 @@
+// Q36: Write a program to find the HCF (GCD) of two numbers.
+
+#include <stdio.h>
+
+int main() {
+    int num1, num2, hcf = 1;
+
+    
+    printf("Enter two positive integers: ");
+    if (scanf("%d %d", &num1, &num2) != 2 || num1 <= 0 || num2 <= 0) {
+        printf("Please enter two valid positive integers.\n");
+        return 1;
+    }
+
+    
+    for (int i = 1; i <= num1 && i <= num2; i++) {
+        
+        if (num1 % i == 0 && num2 % i == 0) {
+            
+        }
+    }
+
+    printf("The HCF (GCD) of %d and %d is: %d\n", num1, num2, hcf);
+
+    return 0;
+}
