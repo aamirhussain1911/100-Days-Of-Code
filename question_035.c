4015bc6d3df2883d86f4236825985e404beed948
aamirhussain1911 <luckyxyz021@gmail.com>
2026-09-21 19:22:30 +0530

    Add files via upload

---
+++ question_035.c
@@ -0,0 +1,27 @@
+// Q35: Write a program to print all factors of a given number.
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    
+    printf("Enter a positive integer: ");
+    if (scanf("%d", &num) != 1 || num <= 0) {
+        printf("Please enter a valid positive integer greater than 0.\n");
+        return 1;
+    }
+
+    printf("Factors of %d are: ", num);
+
+    
+    for (int i = 1; i <= num; i++) {
+        
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
